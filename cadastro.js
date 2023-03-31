87a30323ef7f2ed4104cb4c582bf8646e5825547
Ana Rita Queiroz <anaritadequeiroz@hotmail.com>
2023-03-31 17:49:41 -0300

    atualizando página

--- cadastro.js
+++ cadastro.js
@@ -2,4 +2,6 @@
 function mostra(){
     document.getElementById('informacoes').style.display = 'flex';
 }
-    
+//Receber dados do campo "form_nome_usuario" do formulário
+let nomeUsuario = document.getElementsByName('form_nome_usuário');
+console.log(nomeUsuario);
