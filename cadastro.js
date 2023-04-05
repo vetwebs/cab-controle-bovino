8d1546ddf58ee022227fda2266a0fea4d029cab5
Ana Rita Queiroz <anaritadequeiroz@hotmail.com>
2023-04-05 20:10:32 -0300

    atualizações validações

--- cadastro.js
+++ cadastro.js
@@ -1,7 +1,28 @@
-//Ocultar e Aparecer Divisão Informações Adiconais
+//Ocultar e Aparecer Divisão Informações Adicionais
 function mostra(){
     document.getElementById('informacoes').style.display = 'flex';
 }
-//Receber dados do campo "form_nome_usuario" do formulário
-let nomeUsuario = document.getElementsByName('form_nome_usuário');
-console.log(nomeUsuario);
+//Validação de Cadastro da página login.html
+const formulario = document.querySelector('#form')
+const proprietarioInput = document.querySelector('#proprietario')
+const fazendaInput = document.querySelector('#fazenda')
+
+
+formulario.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+//verificar se o nome está vazio
+    if(proprietarioInput.value ==""){
+        alert('Preencha o campo Proprietário (a).');
+        return;
+    }
+//Verificar se o nome da fazenda está preenchida
+    if(fazendaInput.value==""){
+        alert('Preencha o campo fazenda.');
+        return;
+    }
+//Todos os campos estiverem preenchidos continue
+    formulario.submit();
+    return false;
+})
+
