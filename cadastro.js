56f07f47762fc57b7e5484d7f60543c87409b2b1
Ana Rita Queiroz <anaritadequeiroz@hotmail.com>
2023-04-10 18:15:10 -0300

    criando ações javascript

--- cadastro.js
+++ cadastro.js
@@ -18,3 +18,96 @@ if (fazenda) {
 }
 
 //Capturando as informações preenchidas do cadastro.html para os inputs resumo.html
+const nomeAnimalCadastro = document.getElementById('nomeAnimalCadastro');
+
+const numeroAnimalCadastro = document.getElementById('numeroAnimalCadastro');
+
+const mesesAnimalCadastro = document.getElementById('mesesAnimalCadastro');
+
+const racaAnimalCadastro = document.getElementById('racaAnimalCadastro');
+
+const loteAnimalCadastro = document.getElementById('loteAnimalCadastro');
+
+const categoriaAnimalCadastro = document.getElementById('categoriaAnimalCadastro');
+
+const nomeMaeCadastro = document.getElementById('nomeMaeCadastro');
+
+const racaMaeCadastro = document.getElementById('racaMaeCadastro')
+
+const numeroMaeCadastro = document.getElementById('numeroMaeCadastro')
+
+const loteMaeCadastro = document.getElementById('loteMaeCadastro')
+
+const nomePaiCadastro = document.getElementById('nomePaiCadastro')
+
+const numeroPaiCadastro = document.getElementById('numeroPaiCadastro')
+
+const racaPaiCadastro = document.getElementById('racaPaiCadastro')
+
+const lotePaiCadastro = document.getElementById('lotePaiCadastro')
+
+
+
+nomeAnimalCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('nomeAnimalCadastro', event.target.value);
+});
+
+numeroAnimalCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('numeroAnimalCadastro', event.target.value);
+});
+
+mesesAnimalCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('mesesAnimalCadastro', event.target.value);
+});
+
+racaAnimalCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('racaAnimalCadastro', event.target.value);
+});
+
+loteAnimalCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('loteAnimalCadastro', event.target.value);
+});
+
+categoriaAnimalCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('categoriaAnimalCadastro', event.target.value);
+});
+
+nomeMaeCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('nomeMaeCadastro', event.target.value);
+});
+
+racaMaeCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('racaMaeCadastro', event.target.value);
+});
+
+numeroMaeCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('numeroMaeCadastro', event.target.value);
+});
+
+loteMaeCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('loteMaeCadastro', event.target.value);
+});
+
+nomePaiCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('nomePaiCadastro', event.target.value);
+});
+
+numeroPaiCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('numeroPaiCadastro', event.target.value);
+});
+
+racaPaiCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('racaPaiCadastro', event.target.value);
+});
+
+lotePaiCadastro.addEventListener('input', (event) => {
+  localStorage.setItem('lotePaiCadastro', event.target.value);
+});
+
+
+
+
+
+
+
+
--- resumo.js
+++ resumo.js
@@ -1,16 +1,117 @@
-//Recebendo informações preenchidas na página login.html
-const inputNome = document.getElementById('nome');
-const inputFazenda = document.getElementById('fazenda');
-
-const nome = localStorage.getItem('nome');
-const fazenda = localStorage.getItem('fazenda');
-
-if (nome) {
-  inputNome.value = nome;
-}
-
-if (fazenda) {
-  inputFazenda.value = fazenda;
-}
-
 //Recebendo as informações preenchidas nos campos na página cadastro.html para enviar parar armazenamento 
+const nomeAnimalResumo = document.getElementById('nomeAnimalResumo');
+
+const nomeAnimalCadastro = localStorage.getItem('nomeAnimalCadastro');
+
+if(nomeAnimalCadastro){
+  nomeAnimalResumo.value = nomeAnimalCadastro;
+}
+//Numero Animal
+const numeroAnimalResumo = document.getElementById('numeroAnimalResumo')
+
+const numeroAnimalCadastro = localStorage.getItem('numeroAnimalCadastro');
+
+if(numeroAnimalCadastro){
+  numeroAnimalResumo.value = numeroAnimalCadastro;
+}
+//--------Meses Animal
+const mesesAnimalResumo = document.getElementById('mesesAnimalResumo');
+
+const mesesAnimalCadastro = localStorage.getItem('mesesAnimalCadastro');
+
+if(mesesAnimalCadastro){
+  mesesAnimalResumo.value = mesesAnimalCadastro;
+}
+
+//---------------Raça
+const racaAnimalResumo = document.getElementById('racaAnimalResumo')
+
+const racaAnimalCadastro = localStorage.getItem('racaAnimalCadastro');
+
+if(racaAnimalCadastro){
+  racaAnimalResumo.value = racaAnimalCadastro;
+}
+
+//----------------Lote
+const loteAnimalResumo = document.getElementById('loteAnimalResumo')
+
+const loteAnimalCadastro = localStorage.getItem('loteAnimalCadastro');
+
+if(loteAnimalCadastro){
+  loteAnimalResumo.value = loteAnimalCadastro; 
+}
+//----------------Categoria
+const categoriaAnimalResumo = document.getElementById('categoriaAnimalResumo');
+
+const categoriaAnimalCadastro = localStorage.getItem('categoriaAnimalCadastro');
+
+if(categoriaAnimalCadastro){
+  categoriaAnimalResumo.value = categoriaAnimalCadastro;
+}
+//======DADOS FILIAÇÃO
+//--------Nome Mãe
+const nomeMaeResumo = document.getElementById('nomeMaeResumo');
+
+const nomeMaeCadastro = localStorage.getItem('nomeMaeCadastro')
+
+if(nomeMaeCadastro){
+  nomeMaeResumo.value = nomeMaeCadastro;
+}
+//--------Raça Nome Mãe
+const racaMaeResumo = document.getElementById('racaMaeResumo');
+
+const racaMaeCadastro = localStorage.getItem('racaMaeCadastro');
+
+if(racaMaeCadastro){
+  racaMaeResumo.value = racaMaeCadastro;
+}
+//--------Numero Nome Mãe
+const numeroMaeResumo = document.getElementById('numeroMaeResumo');
+
+const numeroMaeCadastro = localStorage.getItem('numeroMaeCadastro');
+
+if(numeroMaeCadastro){
+  numeroMaeResumo.value = numeroMaeCadastro;
+}
+//-----Lote Mãe
+const loteMaeResumo = document.getElementById('loteMaeResumo');
+
+const loteMaeCadastro = localStorage.getItem('loteMaeCadastro');
+
+if(loteMaeCadastro){
+  loteMaeResumo.value = loteMaeCadastro;
+}
+
+//--------Nome Pai
+const nomePaiResumo = document.getElementById('nomePaiResumo');
+
+const nomePaiCadastro = localStorage.getItem('nomePaiCadastro');
+
+if(nomePaiCadastro){
+  nomePaiResumo.value = nomePaiCadastro;
+}
+//--------Numero Pai
+const numeroPaiResumo = document.getElementById('numeroPaiResumo');
+
+const numeroPaiCadastro = localStorage.getItem('numeroPaiCadastro');
+
+if(numeroPaiCadastro){
+  numeroPaiResumo.value = numeroPaiCadastro;
+}
+//--------Raça Pai
+const racaPaiResumo = document.getElementById('racaPaiResumo');
+
+const racaPaiCadastro = localStorage.getItem('racaPaiCadastro');
+
+if(racaPaiCadastro){
+  racaPaiResumo.value = racaPaiCadastro;
+}
+//--------Lote Pai
+const lotePaiResumo = document.getElementById('lotePaiResumo');
+
+const lotePaiCadastro = localStorage.getItem('lotePaiCadastro');
+
+if(lotePaiCadastro){
+  lotePaiResumo.value = lotePaiCadastro;
+}
+
